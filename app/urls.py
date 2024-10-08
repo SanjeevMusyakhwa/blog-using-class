9929e768d02d2645a8c5d34b2346f952dcab5d5b
sanjeevmusyakhwa <sanjeevmusyakhwa@gmail.com>
2024-10-08 20:06:49 +0545

    Added post detail assets and code changed in views urls etc

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from app import views
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='postList')
+    path('', views.PostListView.as_view(), name='postList'),
+    path('postDetail/<int:pk>/', views.PostDetailView.as_view(), name='postDetail')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from app.models import Post
 # Create your views here.
 
@@ -7,3 +7,17 @@ class PostListView(ListView):
   model = Post
   template_name = 'postList.html'
   context_object_name = "posts"
+
+  # for Filtering 
+  def get_queryset(self):
+    posts = Post.objects.filter(published_at__isnull= False).order_by('-published_at')
+    return posts
+
+class PostDetailView(DetailView):
+  model = Post
+  template_name = 'postDetail.html'
+  context_object_name = 'post'
+
+  def get_queryset(self):
+    post = Post.objects.filter(pk = self.kwargs['pk'], published_at__isnull = False)
+    return post
