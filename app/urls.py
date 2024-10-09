73a72165cf8a5c5b883e7fd2e8da3a277e728e9a
sanjeevmusyakhwa <sanjeevmusyakhwa@gmail.com>
2024-10-09 09:53:40 +0545

    changes made

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('postDetail/<int:pk>/', views.PostDetailView.as_view(), name='postDetail'),
     path('draftList/', views.DraftListView.as_view(), name='draftList'),
     path('draftDetail/<int:pk>/', views.DraftDetailView.as_view(), name='draftDetail'),
+    path('createPost/', views.CreatePostView.as_view(), name='createPost'),
+    path('updatePost/<int:pk>/', views.UpdatePostView.as_view(), name='updatePost'),
+    path('publishPost/<int:pk>/', views.PublishPostView.as_view(), name='publishPost'),
+    path('postDelete/<int:pk>/', views.DeletePostView.as_view(), name='postDelete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy 
+from django.views.generic import ListView,DetailView, CreateView, UpdateView, View
 from app.models import Post
+from app.forms import PostForm
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 # Create your views here.
 
 class PostListView(ListView):
@@ -22,7 +27,7 @@ class PostDetailView(DetailView):
     post = Post.objects.filter(pk = self.kwargs['pk'], published_at__isnull = False)
     return post
   
-class DraftListView(ListView):
+class DraftListView(LoginRequiredMixin,ListView):
   model = Post
   template_name = 'draftList.html'
   context_object_name = 'posts'
@@ -31,7 +36,7 @@ class DraftListView(ListView):
     posts = Post.objects.filter(published_at__isnull = True).order_by('-published_at')
     return posts
 
-class DraftDetailView(DetailView):
+class DraftDetailView(LoginRequiredMixin,DetailView):
   model = Post
   template_name='draftDetail.html'
   context_object_name = 'post'
@@ -39,3 +44,45 @@ class DraftDetailView(DetailView):
   def get_queryset(self):
     post = Post.objects.filter(pk = self.kwargs['pk'], published_at__isnull = True)
     return post
+  
+class CreatePostView(LoginRequiredMixin, CreateView):
+  model = Post
+  template_name = 'postCreate.html'
+  form_class = PostForm
+  # success_url = reverse_lazy("postList")
+
+  def get_success_url(self) -> str:
+    return reverse_lazy('draftDetail', kwargs= {'pk': self.object.pk})
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+  
+class UpdatePostView(LoginRequiredMixin, UpdateView):
+  model = Post
+  template_name = 'postCreate.html'
+  form_class = PostForm
+
+  def get_success_url(self):
+    post = self.get_object()
+    if post.published_at:
+      return reverse_lazy('postDetail', kwargs = {'pk': post.pk})
+    else:
+      return reverse_lazy('draftDetail', kwargs = {'pk': post.pk})
+    
+class PublishPostView(LoginRequiredMixin, View):
+  def get(self, request, pk):
+    post = Post.objects.get(pk = pk , published_at__isnull= True)
+    post.published_at = timezone.now()
+    post.save()
+    return redirect('postDetail', pk)
+  
+class DeletePostView(LoginRequiredMixin, View):
+  def get(self, request, pk):
+    post = Post.objects.get(pk = pk)
+    post.delete()
+    return redirect('postList')
+  
+  def form(self, form):
+    messages.success(self.request, "Post Deleted Successfully")
+    return super().form_valid(form)
